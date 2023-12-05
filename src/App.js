a69d5a106ee8ecdde50df3e9369ed6473569d31e
schoky <danya.safonov.07@mail.ru>
2023-12-05 15:25:04 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import { Header } from "./components/header";
+import { HomePage } from "./pages/home-page";
+import { GamePage } from "./pages/game-page";
+import { OrderPage } from "./pages/order-page";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+function App() {
+  return (
+    <Provider store={ store }>
+      <Router>
+        <div className="App">
+          <Header />
+            <Routes>
+              <Route path="/order" element={ <OrderPage />}/>
+              <Route exact path="/app/:title" element={ <GamePage />}/>
+              <Route exact path="/" element={ <HomePage />}/>
+            </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
